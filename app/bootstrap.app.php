57486ffae56edf1dc78da42531dbd436ef264d5c
gjerokrsteski <gjero@krsteski.de>
2014-02-28 16:28:54 +0100

    speed up app bootstrapping

--- app/bootstrap.app.php
+++ app/bootstrap.app.php
@@ -4,13 +4,14 @@
 | PIMF bootstrap
 |--------------------------------------------------------------------------
 */
-$root = dirname(dirname(__FILE__));
+if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR, true);
+if(!defined('BASE_PATH')) define('BASE_PATH', realpath(dirname(dirname(__FILE__))) . DS, true);
 
-$config = include_once $root.'/app/config.app.php';
+$config = include_once BASE_PATH .'app/config.app.php';
 
-require_once $root.'/pimf-framework/autoload.core.php';
-require_once $root.'/app/autoload.app.php';
-require_once $root.'/pimf-framework/utils.php';
+require_once BASE_PATH .'pimf-framework/autoload.core.php';
+require_once BASE_PATH .'app/autoload.app.php';
+require_once BASE_PATH .'pimf-framework/utils.php';
 
 use \Pimf\Application as App;
 
